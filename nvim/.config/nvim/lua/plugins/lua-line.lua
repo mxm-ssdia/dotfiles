81dbd92585d5f14732b32af9f67be207eaecff0b
mxm <mxm.public.dev@gmail.com>
2025-08-18 00:55:29 +0530

    added cureve :)

--- nvim/.config/nvim/lua/plugins/lua-line.lua
+++ nvim/.config/nvim/lua/plugins/lua-line.lua
@@ -2,13 +2,15 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        require('lualine').setup {
+        require('lualine').setup{
              options = {
                 icons_enabled = true,
                 theme = 'palenight',
                 disabled_filetypes = { 'alpha', 'oil' },
+                component_separators = { left = ')', right = '('},
+                section_separators = { left = '', right = ''},
             },
-            extensions = { 'fugitive' }, -- show git diff
+            extensions = { 'fugitive' },
         }
     end 
 }
