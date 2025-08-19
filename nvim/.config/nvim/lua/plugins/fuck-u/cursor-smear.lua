1d015f89610f1838a7c5694e6f88fe126c40686b
mxm <mxm.public.dev@gmail.com>
2025-08-19 21:40:56 +0530

    updated nothing useful

--- nvim/.config/nvim/lua/plugins/fuck-u/cursor-smear.lua
+++ nvim/.config/nvim/lua/plugins/fuck-u/cursor-smear.lua
@@ -1,4 +1,9 @@
 return {
 	"sphamba/smear-cursor.nvim",
-	opts = {},
+	opts = {
+		stiffness = 0.5,
+		trailing_stiffness = 0.5,
+		distance_stop_animating = 0.5,
+		never_draw_over_target = false,
+	},
 }
---
+++ nvim/.config/nvim/lua/plugins/lsp/eplace-file.lua
@@ -0,0 +1,5 @@
+return {
+	"antosha417/nvim-lsp-file-operations",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = true,
+}
